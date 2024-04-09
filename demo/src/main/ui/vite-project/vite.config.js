39180f6910bdc573e28eaa75172bfe2899733e81
GagoRobi <gagorobi96@gmail.com>
2024-04-09 18:53:16 +0200

    add env variable to vte config for http url requests

--- demo/src/main/ui/vite-project/vite.config.js
+++ demo/src/main/ui/vite-project/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'process';
 
-// https://vitejs.dev/config/
+// Load and merge environment variables
+const mode = process.env.NODE_ENV;
+process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+// Now you can access your environment variables using process.env
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080/'
+      '/api': process.env.VITE_BACKEND_URL
     },
   },
 });
