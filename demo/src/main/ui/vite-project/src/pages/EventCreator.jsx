6dc04f53f25fe80c409b1bc12268e2722c5524ac
GagoRobi <gagorobi96@gmail.com>
2024-04-17 09:16:46 +0200

    refactor event creator form validation

--- demo/src/main/ui/vite-project/src/pages/EventCreator.jsx
+++ demo/src/main/ui/vite-project/src/pages/EventCreator.jsx
@@ -50,19 +50,19 @@ export default function EventCreator() {
                 <Form>
                     <Form.Group className="mb-3" controlId="formTitle">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control as="input" placeholder="Title" onChange={(e) => {
+                        <Form.Control required={true} as="input" placeholder="Title" onChange={(e) => {
                             setTitle(e.target.value)
                         }}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formDescription">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={3} placeholder="Description" onChange={(e) => {
+                        <Form.Control required={true} as="textarea" rows={3} placeholder="Description" onChange={(e) => {
                             setDescription(e.target.value)
                         }}/>
                     </Form.Group>
                     <Form.Label>Type:</Form.Label>
-                    <Form.Select aria-label="Default select example" onChange={(e) => {
+                    <Form.Select required={true} aria-label="Default select example" onChange={(e) => {
                         setType({"id": e.target.value, "name": e.target.selectedOptions[0].innerText})
                     }}>
                         <option selected={true} disabled={true}>Open to select type</option>
@@ -70,16 +70,16 @@ export default function EventCreator() {
                     </Form.Select>
                     <Form.Group className="mb-3" controlId="formDate">
                         <Form.Label>Date:</Form.Label>
-                        <Form.Control onChange={(e) => setDate(e.target.value)} as="input" type={"date"}/>
+                        <Form.Control required={true} onChange={(e) => setDate(e.target.value)} as="input" type={"date"}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formCount">
                         <Form.Label>Head Count:</Form.Label>
-                        <Form.Control onChange={(e) => setHeadcount(e.target.value)} as="input" min={1}
+                        <Form.Control required={true} onChange={(e) => setHeadcount(e.target.value)} as="input" min={1}
                                       type={"number"}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formCount">
                         <Form.Label>Calendar Link:</Form.Label>
-                        <Form.Control onChange={(e) => setLink(e.target.value)} as="input" min={1}/>
+                        <Form.Control required={true} onChange={(e) => setLink(e.target.value)} as="input" min={1}/>
                     </Form.Group>
 
                     <Button variant="primary" onClick={(e) => {
